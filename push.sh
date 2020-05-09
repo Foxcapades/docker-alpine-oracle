f88485137ccb06f63a5763b5882c7b2fc7cf1fec
Elizabeth Harper <foxcapade@gmail.com>
2020-05-09 13:24:33 -0400

    fix push typo

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --pasword-stdin
+echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker push ${IMAGE_NAME}:${TRAVIS_BRANCH}
 docker push ${IMAGE_NAME}:latest
